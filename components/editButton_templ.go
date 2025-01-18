88a9d634c049d0ec7e9bfa7f65a157971aded6d4
CoLdAs1cE <coldasice_t@hotmail.com>
2025-01-18 10:37:23 +0100

    Fix: remove debug

--- components/editButton_templ.go
+++ components/editButton_templ.go
@@ -74,7 +74,7 @@ func EditButton(id string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\">edit</button> <code><pre data-text=\"ctx.signals.JSON()\"></pre></code>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\">edit</button>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
